ef80f128d2031ce722d16779bc1a844806eb072c
Michael <mpierson00@gmail.com>
2024-02-22 22:30:02 -0500

    Added comments

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var length = prompt("Enter the desired length of your password (8-128 characters):");
+  if (length < 8 || length > 128 || isNaN(length)) {
+    alert("Password length must be a number between 8 and 128.");
+  }
+  // Password option alerts
+  var includeUppercase = confirm("Would you like to include uppercase letters? Click OK for yes, Cancel for no.");
+  var includeLowercase = confirm("Would you like to include lowercase letters? Click Ok for yes, Cancel for no.");
+  var includeNumbers = confirm("Would you like to include numbers? Click Ok for yes, Cancel for no.");
+  var includeSymbols = confirm("Would you like to include special characters? Click Ok for yes, Cancel for no.");
+
+  var password = generatePassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,4 +21,21 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols) {
+  var charset = "";
+  var password = "";
+  // Password selection options
+  if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+  if (includeNumbers) charset += "0123456789";
+  if (includeSymbols) charset += "!@#$%^&*()_+`'|}{[]:;?><,./-=";
+
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex)
+  }
+
+  return password;
+}
